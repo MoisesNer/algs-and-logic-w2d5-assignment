08428150586fca68baef9f1497f6d28e0052264d
Moises Nervis <Nervis@Macs-iMac-2.local>
2020-09-27 10:51:09 -0700

    isPrime

--- isPrime.js
+++ isPrime.js
@@ -1,5 +1,12 @@
 // check if a given number is prime. return true or false. 
 
 const isPrime = function(num) {
-  // your code here
+  for(let m = 2; m < num; m++) {
+    if(num % m === 0) {
+      console.log(num + " is dibiisle by " + m);
+      return false;
+    }
+  }
+  return true;
 }
+console.log(isPrime(101));
